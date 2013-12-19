000b8df5fc7cf63006df2b8463387031cdfa7660
Atul Varma <varmaa@gmail.com>
2013-12-18 19:56:38 -0500

    replaced Backbone.Events with DOM CustomEvent usage.

--- cooa.js
+++ cooa.js
@@ -29,110 +29,13 @@ var COOA = (function() {
     }
   };
 
-  var Events = COOA.Events = {
-    // This was lifted from Backbone.Events.
-
-    // Regular expression used to split event strings
-    eventSplitter: /\s+/,
-
-    // Bind one or more space separated events, `events`, to a `callback`
-    // function. Passing `"all"` will bind the callback to all events fired.
-    on: function(events, callback, context) {
-
-      var calls, event, node, tail, list;
-      if (!callback) return this;
-      events = events.split(this.eventSplitter);
-      calls = this._callbacks || (this._callbacks = {});
-
-      // Create an immutable callback list, allowing traversal during
-      // modification.  The tail is an empty object that will always be used
-      // as the next node.
-      while (event = events.shift()) {
-        list = calls[event];
-        node = list ? list.tail : {};
-        node.next = tail = {};
-        node.context = context;
-        node.callback = callback;
-        calls[event] = {tail: tail, next: list ? list.next : node};
-      }
-
-      return this;
-    },
-
-    // Remove one or many callbacks. If `context` is null, removes all
-    // callbacks with that function. If `callback` is null, removes all
-    // callbacks for the event. If `events` is null, removes all bound
-    // callbacks for all events.
-    off: function(events, callback, context) {
-      var event, calls, node, tail, cb, ctx;
-
-      // No events, or removing *all* events.
-      if (!(calls = this._callbacks)) return;
-      if (!(events || callback || context)) {
-        delete this._callbacks;
-        return this;
-      }
-
-      // Loop through the listed events and contexts, splicing them out of the
-      // linked list of callbacks if appropriate.
-      events = events ? events.split(this.eventSplitter) : Object.keys(calls);
-      while (event = events.shift()) {
-        node = calls[event];
-        delete calls[event];
-        if (!node || !(callback || context)) continue;
-        // Create a new list, omitting the indicated callbacks.
-        tail = node.tail;
-        while ((node = node.next) !== tail) {
-          cb = node.callback;
-          ctx = node.context;
-          if ((callback && cb !== callback) || (context && ctx !== context)) {
-            this.on(event, cb, ctx);
-          }
-        }
-      }
-
-      return this;
-    },
-
-    // Trigger one or many events, firing all bound callbacks. Callbacks are
-    // passed the same arguments as `trigger` is, apart from the event name
-    // (unless you're listening on `"all"`, which will cause your callback to
-    // receive the true name of the event as the first argument).
-    trigger: function(events) {
-      var event, node, calls, tail, args, all, rest;
-      if (!(calls = this._callbacks)) return this;
-      all = calls.all;
-      events = events.split(this.eventSplitter);
-      rest = Array.prototype.slice.call(arguments, 1);
-
-      // For each event, walk through the linked list of callbacks twice,
-      // first to trigger the event, then to trigger any `"all"` callbacks.
-      while (event = events.shift()) {
-        if (node = calls[event]) {
-          tail = node.tail;
-          while ((node = node.next) !== tail) {
-            node.callback.apply(node.context || this, rest);
-          }
-        }
-        if (node = all) {
-          tail = node.tail;
-          args = [event].concat(rest);
-          while ((node = node.next) !== tail) {
-            node.callback.apply(node.context || this, args);
-          }
-        }
-      }
-
-      return this;
-    },
-
-    mixin: function(target) {
-      ['on', 'off', 'trigger', 'eventSplitter'].forEach(function(name) {
-        target[name] = this[name];
-      }, this);
-      return target;
-    }
-  };
+  var CustomEvent = function CustomEvent(type, params) {
+      params = params || {bubbles: false, cancelable: false};
+      var event = document.createEvent('CustomEvent');
+      event.initCustomEvent(type, params.bubbles, params.cancelable,
+                            params.detail);
+      return event;
+    };
 
   var Story = COOA.Story = function Story(options) {
     function $(selector) {
@@ -185,23 +88,28 @@ var COOA = (function() {
     var parent = options.parent;
     var globalParent = options.globalParent || parent;
     var storage = options.storage || Storage({});
-    var self = Events.mixin({
+    var self = {
       $: $,
+      parent: parent,
       showSection: showSection,
       setDebugMode: setDebugMode,
       storage: storage
-    });
+    };
 
     highlightBrokenLinks();
     globalParent.addEventListener('click', function(e) {
       if (e.target.nodeName == 'A') {
         var href = e.target.getAttribute('href');
+        var changeEvent;
 
         if (!(href && href[0] == '#')) return;
-        self.trigger('sectionchange', {
-          href: href,
-          preventDefault: e.preventDefault.bind(e)
+        changeEvent = CustomEvent('cooasectionchange', {
+          bubbles: false,
+          cancelable: true,
+          detail: {href: href}
         });
+        parent.dispatchEvent(changeEvent);
+        if (changeEvent.defaultPrevented) e.preventDefault();
       }
     }, true);
     if (options.debugCheckbox) hookupDebugCheckbox(options.debugCheckbox);
@@ -240,11 +148,11 @@ var COOA = (function() {
       // We might be in jsbin or thimble or another two-pane editor,
       // which often handle named anchors poorly, so we'll handle
       // them ourselves.
-      story.on('sectionchange', function(e) {
-        story.showSection(e.href);
-        storage.set('cooa-hash', e.href);
+      story.parent.addEventListener('cooasectionchange', function(e) {
+        story.showSection(e.detail.href);
+        storage.set('cooa-hash', e.detail.href);
         e.preventDefault();
-      });
+      }, false);
 
       // Two-pane editors don't always retain scroll position, so we'll
       // do that ourselves too.
